c50301268be687478cdb1250c50975fbf5e54aac
joshcard <66044735+joshcard@users.noreply.github.com>
2021-03-16 17:37:40 -0600

    setting up some models

---
+++ Models/GroupInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempleToursProject.Models
+{
+    public class GroupInfo
+    {
+        [Required]
+        public string GroupName { get; set; }
+
+        [Required]
+        public string GroupSize { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+    }
+}
